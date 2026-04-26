ea266be2d98c63f6b0acfb162ea412fcf6ecf031
Kaidan Theron <kaidan13th@gmail.com>
2026-04-26 13:06:59 +0200

    update seeding

--- apps/backend/src/database/seeds/seed.data.ts
+++ apps/backend/src/database/seeds/seed.data.ts
@@ -1,8 +1,10 @@
-/**
- * Plain South African ID numbers used only for seeding.
- */
 export const identitySeedPlainSaIds: readonly string[] = [
     "0501018431087",
     "0501017561082",
     "0501016580083",
 ];
+
+export const practiceNames: readonly string[] = [
+    "Winelands Family Practice",
+    "Intercare Panorama"
+]
--- apps/backend/src/database/seeds/seed.ts
+++ apps/backend/src/database/seeds/seed.ts
@@ -2,30 +2,50 @@ import "reflect-metadata";
 import dataSource from "../../config/database.config";
 import { PatientIdentityEntity } from "../../modules/registration/infrastructure/persistence/typeorm/entities/patient-identity.entity";
 import { CryptoHasher } from "../../modules/registration/infrastructure/security/crypto-hasher";
-import { identitySeedPlainSaIds } from "./identity.seed.data";
+import { identitySeedPlainSaIds, practiceNames } from "./seed.data";
+import { PracticeEntity } from "src/modules/registration/infrastructure/persistence/typeorm/entities/practice.entity";
 
 async function main(): Promise<void> {
     const hasher = new CryptoHasher();
     await dataSource.initialize();
 
     try {
-        const repo = dataSource.getRepository(PatientIdentityEntity);
+        const patientIdentities = dataSource.getRepository(PatientIdentityEntity);
         let insertedCount = 0;
         let skippedCount = 0;
 
         for (const plain of identitySeedPlainSaIds) {
             const hashedIdentity = await hasher.hash(plain);
-            const already = await repo.exists({ where: { identity: hashedIdentity } });
+            const already = await patientIdentities.exists({ where: { identity: hashedIdentity } });
             if (already) {
                 skippedCount += 1;
                 continue;
             }
-            await repo.insert({ identity: hashedIdentity });
+            await patientIdentities.insert({ identity: hashedIdentity });
             insertedCount += 1;
         }
 
         console.log(
             `Identity seeding complete. Inserted: ${insertedCount}, skipped: ${skippedCount}.`,
+        );
+
+        const practices = dataSource.getRepository(PracticeEntity)
+        insertedCount = 0;
+        skippedCount = 0;
+
+        for (const practice of practiceNames) {
+            const already = await practices.exists({ where: { name: practice }});
+            if (already) {
+                skippedCount += 0;
+                continue;
+            }
+            await practices.insert({ name: practice });
+            insertedCount += 1;
+        }
+
+
+        console.log(
+            `Practice seeding complete. Inserted: ${insertedCount}, skipped: ${skippedCount}.`,
         );
     } finally {
         if (dataSource.isInitialized) {
